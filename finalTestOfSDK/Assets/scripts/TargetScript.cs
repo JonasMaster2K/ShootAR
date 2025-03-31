b6353ecf38b78e655d5a5f5e1fbfff2774a2d304
Lukas M. Munz <lukas_martin.munz@smail.th-koeln.de>
2025-03-31 14:55:20 +0200

    Update TargetScript.cs

--- finalTestOfSDK/Assets/scripts/TargetScript.cs
+++ finalTestOfSDK/Assets/scripts/TargetScript.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class TargetScript : MonoBehaviour
 {
+    private bool hasFallen = false;
+    private Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>() ?? gameObject.AddComponent<Rigidbody>();
+        rb.isKinematic = true;  // Physik-Effekte ausschalten
+        rb.useGravity = false;  // Schwerkraft aus, da isKinematic aktiv ist
     }
 
     // Update is called once per frame
@@ -17,12 +21,24 @@ public class TargetScript : MonoBehaviour
 
         private void OnCollisionEnter(Collision other)
     {
-
-        //TODO change to targets
-        if (other.gameObject.tag =="Ball")
+        if (!hasFallen && other.gameObject.CompareTag("Ball"))
         {
-            Destroy(gameObject);
-            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            hasFallen = true;
+
+            // Sicherstellen, dass ein Rigidbody vorhanden ist
+            Rigidbody rb = GetComponent<Rigidbody>() ?? gameObject.AddComponent<Rigidbody>();
+            rb.isKinematic = false;
+
+            rb.useGravity = true;
+
+            // Kraft nach hinten zum Umfallen
+            rb.AddForce(-transform.forward * 15f, ForceMode.Impulse);
+
+            // Ballkollision deaktivieren
+            other.gameObject.GetComponent<Collider>().enabled = false;
+
+            // Zerstören nach kurzer Verzögerung
+            Destroy(gameObject, 2f);
         }
     }
 }
